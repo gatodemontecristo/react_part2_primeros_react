e87724693b6ef03ceaecaecf962ed0d4b795887f
Erick Dejo Vicente <61488294+mapachefurioso@users.noreply.github.com>
2023-08-15 16:59:39 -0500

    Tarea nuevo componente

---
+++ src/FirstApp.jsx
@@ -0,0 +1,7 @@
+
+
+export const FirstApp = () => {
+  return (
+    <h2>My First App</h2>
+  )
+}
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,11 @@ import React from "react";
 import ReactDOM from 'react-dom/client';
 
 import {HelloWorldComp,hola} from './HelloWorldApp';
+import {FirstApp} from "./FirstApp";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
         <HelloWorldComp/>
+        <FirstApp/>
     </React.StrictMode>
 );
