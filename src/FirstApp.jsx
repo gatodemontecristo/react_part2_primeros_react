6fdb41fbdebdda75caa5e58e36310258ed311b0f
Erick Dejo Vicente <61488294+mapachefurioso@users.noreply.github.com>
2023-08-15 18:22:43 -0500

    Componentes y estilos CSS

--- src/FirstApp.jsx
+++ src/FirstApp.jsx
@@ -1,7 +1,19 @@
+import { Fragment } from "react";
 
+const newMessage = {
+  message: "Hola Mundo",
+  title: "Fernando",
+};
+const getMensajito = (a, b) => {
+  return `La operacion mas basica es de ${a + b}`;
+};
 
 export const FirstApp = () => {
   return (
-    <h2>My First App</h2>
-  )
-}
+    <Fragment>
+      <h1>{getMensajito(3, 4)}</h1>
+      {/* <code>{JSON.stringify(newMessage)}</code> */}
+      <p>Spy un subtitulo</p>
+    </Fragment>
+  );
+};
