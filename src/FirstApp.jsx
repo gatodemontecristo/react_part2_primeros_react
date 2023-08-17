e39584c813ecf38b4f7f0e93a6ed0d4a8fb26c17
Erick Dejo Vicente <61488294+mapachefurioso@users.noreply.github.com>
2023-08-16 21:24:41 -0500

    PROPS en general

--- src/FirstApp.jsx
+++ src/FirstApp.jsx
@@ -1,4 +1,5 @@
 import { Fragment } from "react";
+import PropTypes from "prop-types"
 
 const newMessage = {
   message: "Hola Mundo",
@@ -8,12 +9,28 @@ const getMensajito = (a, b) => {
   return `La operacion mas basica es de ${a + b}`;
 };
 
-export const FirstApp = () => {
+export const FirstApp = ({title, subTitle, name}) => {
+
+
   return (
     <Fragment>
+      <h1>{title}</h1>
+      <p>{subTitle}</p>
+      <p>{name}</p>
       <h1>{getMensajito(3, 4)}</h1>
       {/* <code>{JSON.stringify(newMessage)}</code> */}
       <p>Spy un subtitulo</p>
     </Fragment>
   );
-};
+}
+
+FirstApp.propTypes = {
+  title: PropTypes.string.isRequired,
+  subTitle: PropTypes.number.isRequired
+}
+
+FirstApp.defaultProps = {
+  title: "No hya ningun titulo",
+  subTitle: "No hya ningun subtitulo",
+  name: "Fernando Herrera"
+}
--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import ReactDOM from 'react-dom/client';
-
 import {HelloWorldComp,hola} from './HelloWorldApp';
 import {FirstApp} from "./FirstApp";
 import './styles.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
+
     <React.StrictMode>
         <HelloWorldComp/>
-        <FirstApp/>
+        <FirstApp  subTitle={123}/>
     </React.StrictMode>
 );
