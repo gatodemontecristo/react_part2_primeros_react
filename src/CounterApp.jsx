8647880fcb20edf79e48838259c424205e456d82
Erick Dejo Vicente <61488294+mapachefurioso@users.noreply.github.com>
2023-08-16 21:43:45 -0500

    Tarea CounterApp

---
+++ src/CounterApp.jsx
@@ -0,0 +1,16 @@
+import { Fragment } from "react";
+import PropTypes from "prop-types";
+
+
+export const CounterApp = ({value}) => {
+  return (
+    <Fragment>
+        <h1>CounterApp</h1>
+        <h2>{value}</h2>
+    </Fragment>
+  )
+}
+
+CounterApp.propTypes = {
+    value: PropTypes.number.isRequired
+}
--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import ReactDOM from 'react-dom/client';
 import {HelloWorldComp,hola} from './HelloWorldApp';
 import {FirstApp} from "./FirstApp";
+import { CounterApp } from "./CounterApp";
 import './styles.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
 
     <React.StrictMode>
         <HelloWorldComp/>
-        <FirstApp  subTitle={123}/>
+        {/* <FirstApp  subTitle={123}/> */}
+        <CounterApp value={999}></CounterApp>
     </React.StrictMode>
 );
