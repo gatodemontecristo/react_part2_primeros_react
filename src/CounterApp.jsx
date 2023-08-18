1504e3e695f794e75909b51f9d4ee0fae746190f
Erick Dejo Vicente <61488294+mapachefurioso@users.noreply.github.com>
2023-08-17 21:49:05 -0500

    evento click

--- src/CounterApp.jsx
+++ src/CounterApp.jsx
@@ -1,16 +1,21 @@
 import { Fragment } from "react";
 import PropTypes from "prop-types";
 
+export const CounterApp = ({ value }) => {
+  function handleAdd(event, newValue) {
+    // console.log(event);
+    console.log(newValue);
+  }
 
-export const CounterApp = ({value}) => {
   return (
     <Fragment>
-        <h1>CounterApp</h1>
-        <h2>{value}</h2>
+      <h1>CounterApp</h1>
+      <h2>{value}</h2>
+      <button onClick={(event) => handleAdd(event, "hola")}>+1</button>
     </Fragment>
-  )
-}
+  );
+};
 
 CounterApp.propTypes = {
-    value: PropTypes.number.isRequired
-}
+  value: PropTypes.number.isRequired,
+};
