62dcd96171739327e37698a64038479a0cb57ced
Erick Dejo Vicente <61488294+mapachefurioso@users.noreply.github.com>
2023-08-25 00:44:16 -0500

    Test ALL

--- tests/demo.test.js
+++ tests/demo.test.js
@@ -1,8 +1,17 @@
 
+describe('Pruenbas en <DemoComponent>', () => { 
+    test('Esta prueba no debe de fallar', ()=>{
+   
+        // 1. inicializacion
+        const message1 = 'Hola Mundo';
+    
+        // 2. estimulo
+        const message2 = message1.trim();
+    
+        // 3. observar el comportamiento... esperado
+        expect(message1).toBe(message2);
+    
+    
+    })
+ })
 
-
-test('Esta rpueba no debe de fallar', ()=>{
-    if(1===0){
-        throw new Error('No puede dividir entre cero');
-    } 
-})
