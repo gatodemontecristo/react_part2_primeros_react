f29acc59298a5b9a7cce6831947172f35ca7bfb2
Erick Dejo Vicente <61488294+mapachefurioso@users.noreply.github.com>
2023-09-03 23:51:18 -0500

    Pruebas con componentes REACT

--- src/main.jsx
+++ src/main.jsx
@@ -8,8 +8,8 @@ import './styles.css';
 ReactDOM.createRoot(document.getElementById('root')).render(
 
     <React.StrictMode>
-        <HelloWorldComp/>
-        {/* <FirstApp  subTitle={123}/> */}
-        <CounterApp value={20}></CounterApp>
+        {/* <HelloWorldComp/> */}
+        <FirstApp  subTitle={123}/>
+        {/* <CounterApp value={20}></CounterApp> */}
     </React.StrictMode>
 );
